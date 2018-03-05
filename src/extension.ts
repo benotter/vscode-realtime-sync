e468be7e32a7e94083e46c9d3c25e951a32756fa
Ben Otter <benotter@me.com>
2018-03-04 19:53:51 -0800

    Reset

--- src/extension.ts
+++ src/extension.ts
@@ -1,74 +1,10 @@
 import * as code from 'vscode';
 
-import { RSServer } from './lib/rs-server/rs-server';
-import { RSClient } from './lib/rs-client/rs-client';
-import { RSStatusBar } from './lib/ui/rs-status-bar';
 
-let server: RSServer;
-let client: RSClient;
-let statusBar: RSStatusBar;
 
 export function activate ( context: code.ExtensionContext )
 {
-    let {
-        realtime_sync: {
-            serverPort = 8117,
-            clientPort = 8117,
-            // userName = null,
-        } = {}
-    } = code.workspace.getConfiguration( 'realtime-sync' );
-
-    statusBar = new RSStatusBar( code.window.createStatusBarItem( code.StatusBarAlignment.Right ) );
-
-    let startUpDisp = code.commands.registerCommand( 'extension.startUpRS', () =>
-    {
-        server = new RSServer();
-        client = new RSClient();
-    } );
-
-    let startServerDisp = code.commands.registerCommand( 'extension.startRSServer', () => 
-    {
-        server.start( serverPort );
-        statusBar.setServer();
-    } );
-
-    let stopServerDisp = code.commands.registerCommand( 'extension.stopRSServer', () => 
-    {
-        server.stop();
-        statusBar.setIdle();
-    } );
-
-    let joinServerDisp = code.commands.registerCommand( 'extension.joinRSServer', () =>
-    {
-        code.window.showInputBox( { placeHolder: "localhost" } )
-            .then( addr =>
-            {
-                if ( !addr )
-                    return;
-
-                client.join( addr, clientPort );
-                statusBar.setClient();
-            } );
-    } );
-
-    let leaveServerDisp = code.commands.registerCommand( 'extension.leaveRSServer', () =>
-    {
-        client.leave();
-        statusBar.setIdle();
-    } );
-
-    let addFileDisp = code.commands.registerCommand( 'extension.addFileToRS', () => { } );
-    let remFileDisp = code.commands.registerCommand( 'extension.remFileFromRS', () => { } );
-
-    context.subscriptions.push(
-        startUpDisp,
-        startServerDisp,
-        stopServerDisp,
-        addFileDisp,
-        remFileDisp,
-        joinServerDisp,
-        leaveServerDisp,
-    );
+    
 }
 
 export function deactivate ()
--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,9 +1,3 @@
-import
-{
-    ERSServerMessageType, RSServerMessages,
-    ERSClientMessageType, RSClientMessages,
-} from './rs-i-types';
-
 export namespace SafeJSON 
 {
     export function parse<T = any>( str: string, def: T ): T
@@ -15,31 +9,5 @@ export namespace SafeJSON
     export function stringify ( obj: any, prettyPrint: boolean = false ): string
     {
         return JSON.stringify( obj, void 0, prettyPrint ? ' ' : void 0 );
-    }
-}
-
-export namespace RSMessageUtils 
-{
-    export function formatMess (
-        mess: RSClientMessages.IRSClientMessage | RSServerMessages.IRSServerMessage
-    ): string 
-    {
-        return SafeJSON.stringify( mess );
-    }
-
-    export function getBaseClientMess<T = RSClientMessages.IRSClientMessage>(
-        type: ERSClientMessageType,
-        data: T,
-    ): T
-    {
-        return Object.assign( { type, }, data );
-    }
-
-    export function getBaseServerMess<T = RSServerMessages.IRSServerMessage>(
-        type: ERSServerMessageType,
-        data: T,
-    ): T
-    {
-        return Object.assign( { type, }, data );
     }
 }
